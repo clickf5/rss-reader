2d8dd5aea850fe1f763f7578e7951e058e2a0267
clickf5 <paninhome@gmail.com>
2020-04-28 08:01:10 +0300

    add new test

--- __tests__/application.test.js
+++ __tests__/application.test.js
@@ -1,6 +1,7 @@
 import { promises as fs } from 'fs';
 import path from 'path';
 import { html } from 'js-beautify';
+import userEvent from '@testing-library/user-event';
 
 import app from '../src/js/application';
 
@@ -12,15 +13,26 @@ const htmlOptions = {
 const fixturesPath = path.join(__dirname, '__fixtures__');
 const getTree = () => html(document.body.innerHTML, htmlOptions);
 
+let elements;
+
 beforeEach((done) => {
   fs.readFile(path.join(fixturesPath, 'index.html')).then((data) => {
     const initHTML = data.toString();
     document.documentElement.innerHTML = initHTML;
     app();
+    elements = {
+      urlInput: document.querySelector('[name="url"]'),
+    };
     done();
   });
 });
 
 test('application', () => {
   expect(getTree()).toMatchSnapshot();
+
+  userEvent.type(elements.urlInput, 'wrong url', { allAtOnce: true })
+    .then(() => {
+      elements.urlInput.setAttribute('value', 'wrong url');
+      expect(getTree()).toMatchSnapshot();
+    });
 });
