b4b2e613506cdb9e1f31b37b6651fbf972c071e1
clickf5 <paninhome@gmail.com>
2020-04-28 08:46:56 +0300

    add state and base elements

--- src/js/application.js
+++ src/js/application.js
@@ -1,5 +1,21 @@
 const app = () => {
-  console.log('app is running!');
+  const state = {
+    form: {
+      processState: 'filling',
+      processError: null,
+      fields: {
+        url: '',
+      },
+      valid: true,
+      errors: {},
+    }
+  };
+
+  const form = document.querySelector('.rss-form');
+  const elements = {
+    url: document.querySelector('[name="url"]'),
+  };
+  const submitButton = form.querySelector('button[type="submit"]');
 };
 
 export default app;
