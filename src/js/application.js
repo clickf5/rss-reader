043269e62f878492bf589f8e5ed9fea53a695cbb
clickf5 <paninhome@gmail.com>
2020-05-04 14:01:45 +0300

    fix snapshot

--- src/js/application.js
+++ src/js/application.js
@@ -1,3 +1,25 @@
+/* eslint no-param-reassign: ["error", { "props": false }] */
+import * as yup from 'yup';
+import _ from 'lodash';
+import setWatchers from './view';
+
+const schema = yup.object().shape({
+  url: yup.string().required().url(),
+});
+
+const updateValidationState = (state) => {
+  try {
+    schema.validateSync(state.form.fields, { abortEarly: false });
+    state.form.valid = true;
+    state.form.errors = {};
+  } catch (e) {
+    const errors = _.keyBy(e.inner, 'path');
+    console.log(errors);
+    state.form.valid = false;
+    state.form.errors = errors;
+  }
+};
+
 const app = () => {
   const state = {
     form: {
@@ -8,14 +30,23 @@ const app = () => {
       },
       valid: true,
       errors: {},
-    }
+    },
   };
 
-  const form = document.querySelector('.rss-form');
-  const elements = {
-    url: document.querySelector('[name="url"]'),
+  const ui = {
+    form: document.querySelector('.rss-form'),
+    elements: {
+      url: document.querySelector('input[name="url"]'),
+    },
+    submitButton: document.querySelector('button[type="submit"]'),
   };
-  const submitButton = form.querySelector('button[type="submit"]');
+
+  ui.elements.url.addEventListener('input', (e) => {
+    state.form.fields.url = e.target.value;
+    updateValidationState(state);
+  });
+
+  setWatchers(state, ui);
 };
 
 export default app;
--- src/js/view.js
+++ src/js/view.js
@@ -1,3 +1,28 @@
-export const setWatchers = (state) => {
+import { watch } from 'melanke-watchjs';
 
+const setWatchers = (state, ui) => {
+  watch(state.form, 'processState', () => {
+    const { processState } = state.form;
+    const { submitButton } = ui;
+    switch (processState) {
+      case 'failed':
+        submitButton.disabled = false;
+        break;
+      case 'filling':
+        submitButton.disabled = false;
+        break;
+      case 'sending':
+        submitButton.disabled = true;
+        break;
+      default:
+        throw new Error(`Unknown state: ${processState}`);
+    }
+  });
+
+  watch(state.form, 'valid', () => {
+    const { submitButton } = ui;
+    submitButton.disabled = !state.form.valid;
+  });
 };
+
+export default setWatchers;
